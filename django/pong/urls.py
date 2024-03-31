4a9e748471da99142a64662ac68a38e25bada3d3
gabrielclcardoso <gabrielclcardoso@gmail.com>
2024-03-31 18:32:29 -0300

    feat: set initial structure

--- django/pong/urls.py
+++ django/pong/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from . import views
+from pong import views
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('main/', views.main, name='main'),
+    path('game-menu/', views.gameMenu, name='gameMenu'),
+    path('local-game/', views.localGame, name='localGame'),
+    path('online-game/', views.onlineGame, name='onlineGame'),
 ]
--- django/pong/views.py
+++ django/pong/views.py
@@ -1,82 +1,39 @@
 from django.shortcuts import render
-import pong.serializers as serializers
-import pong.models as models
-from rest_framework import viewsets, permissions
-from rest_framework.permissions import IsAuthenticated
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework.decorators import permission_classes, action
-
-# Create your views here.
 
 
 def index(request):
     if request.method == "GET":
-        return render(request, "index.html")
+        return render(request, "pong/index.html")
 
 
 def main(request):
     if request.method == "GET":
-        return render(request, "main.html")
+        return render(request, "pong/main.html")
 
 
-@permission_classes((permissions.AllowAny,))
-class GameView(APIView):
-    def get(self, request, format=None):
-        return render(request, "game.html")
+def gameMenu(request):
+    if request.method == "GET":
+        return render(request, "pong/game_menu.html")
 
 
-@permission_classes((permissions.AllowAny,))
-class Menu(APIView):
-    def get(self, request, format=None):
-        return render(request, "menu.html")
+def localGame(request):
+    if request.method == "GET":
+        return render(request, "pong/local_game.html")
 
 
-@permission_classes((permissions.AllowAny,))
-class Game(APIView):
-    def get(self, request, format=None):
-        return render(request, "game.html")
+def onlineGame(request):
+    if request.method == "GET":
+        return render(request, "pong/online_game.html")
 
-
-@permission_classes((permissions.AllowAny,))
-class Loadscreen(APIView):
-    def get(self, request, format=None):
-        return render(request, "loadscreen.html")
-
-
-@permission_classes((permissions.AllowAny,))
-class Leaderboard(APIView):
-    def get(self, request, format=None):
-        return render(request, "leaderboard.html")
-
-
-@permission_classes((permissions.AllowAny,))
-class Cadastro(APIView):
-    def get(self, request, format=None):
-        return render(request, "cadastro.html")
-
-
-class UserViewSet(viewsets.ModelViewSet):
-    queryset = models.User.objects.all()
-    serializer_class = serializers.UserSerializer
-    permission_classes = [IsAuthenticated]
-
-
-class GameViewSet(viewsets.ModelViewSet):
-    queryset = models.Game.objects.all()
-    serializer_class = serializers.GameSerializer
-    permission_classes = [IsAuthenticated]
-
-
-class TournamentViewSet(viewsets.ModelViewSet):
-    queryset = models.Tournament.objects.all()
-    lookup_field = 'name'
-    serializer_class = serializers.TournamentSerializer
-    permission_classes = [IsAuthenticated]
-
-    # temporary API endpoint to advance tournament
-    @action(detail=True, methods=['GET'])
-    def advance(self, request, name=None):
-        tournament = self.get_object()
-        tournament.new_round()
-        return Response({'status': 'tournament round advanced'})
+# class TournamentViewSet(viewsets.ModelViewSet):
+#     queryset = models.Tournament.objects.all()
+#     lookup_field = 'name'
+#     serializer_class = serializers.TournamentSerializer
+#     permission_classes = [IsAuthenticated]
+#
+#     # temporary API endpoint to advance tournament
+#     @action(detail=True, methods=['GET'])
+#     def advance(self, request, name=None):
+#         tournament = self.get_object()
+#         tournament.new_round()
+#         return Response({'status': 'tournament round advanced'})
