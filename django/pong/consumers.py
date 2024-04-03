df4c573d94b9ffef26449afc137ff3f758f7d51c
gabrielclcardoso <gabrielclcardoso@gmail.com>
2024-04-03 13:25:44 -0300

    feat: implement bar position communication through socket

--- django/pong/consumers.py
+++ django/pong/consumers.py
@@ -1,21 +1,75 @@
 from channels.generic.websocket import AsyncWebsocketConsumer
 import json
+import asyncio
 
 
 class localGameCosumer(AsyncWebsocketConsumer):
+    BAR_UNIT = 1
+    BAR_UPPERBOUND = 80
+    BAR_LOWERBOUND = 20
 
     async def connect(self):
+        await self.set_initial_values()
         await self.accept()
 
     async def disconnect(self, close_code):
+        self.interval_task.cancel()
         pass
 
     async def receive(self, text_data):
         data_json = json.loads(text_data)
-        direction = data_json['move']
+        if 'start' in data_json:
+            self.interval_task = asyncio.create_task(self.update_game())
+            return
+        elif 'stop' in data_json and self.interval_task:
+            self.interval_task.cancel()
+            await self.set_initial_values()
+            return
+        try:
+            p1_direction = data_json['l']
+            p2_direction = data_json['r']
+        except KeyError:
+            await self.send(text_data=json.dumps(
+                {"status": "invalid", "message": "Invalid JSON received"}
+            ))
+            return
+        self.p1_move = p1_direction
+        self.p2_move = p2_direction
         await self.send(text_data=json.dumps(
-            {"move": direction}
+            {"status": "received"}
         ))
+
+    async def update_game(self):
+        while True:
+            await asyncio.sleep(0.016)
+            if self.p1_move:
+                if self.p1_move == 'u' and self.p1_pos < self.BAR_UPPERBOUND:
+                    self.p1_pos += self.BAR_UNIT
+                elif self.p1_move == 'd' and self.p1_pos > self.BAR_LOWERBOUND:
+                    self.p1_pos -= self.BAR_UNIT
+                self.p1_move = None
+            if self.p2_move:
+                if self.p2_move == 'u' and self.p2_pos < self.BAR_UPPERBOUND:
+                    self.p2_pos += self.BAR_UNIT
+                elif self.p2_move == 'd' and self.p2_pos > self.BAR_LOWERBOUND:
+                    self.p2_pos -= self.BAR_UNIT
+                self.p2_move = None
+            try:
+                await self.send(text_data=json.dumps(
+                    {
+                        "p1": self.p1_pos,
+                        "p2": self.p2_pos
+                    }
+                ))
+            except Exception:
+                break
+
+    async def set_initial_values(self):
+        self.p1_pos = 50
+        self.p2_pos = 50
+        self.p1_move = None
+        self.p2_move = None
+        self.interval_task = None
 
 
 class onlineGameCosumer(AsyncWebsocketConsumer):
--- django/pong/static/pong/js/gameWebSocket.js
+++ django/pong/static/pong/js/gameWebSocket.js
@@ -1,20 +1,108 @@
 class GameWebSocket {
+  P1_UP = "w";
+  P1_DOWN = "s";
+  P2_UP = "ArrowUp";
+  P2_DOWN = "ArrowDown";
+  keyDownListener = null;
+  keyUpListener = null;
+  p1Direction = null;
+  p2Direction = null;
+  gameRunning = false;
+
   constructor() {
     this.socket = new WebSocket(
       "ws://" + window.location.host + "/ws/local-game/",
     );
     this.socket.onmessage = this.onMessage.bind(this);
     this.socket.onclose = this.onClose.bind(this);
+    this.handleKeyUp = this.handleKeyUp.bind(this);
+    this.handleKeyDown = this.handleKeyDown.bind(this);
   }
 
   onMessage(event) {
     const data = JSON.parse(event.data);
-    console.log(data);
+    if (this.gameRunning && !("status" in data)) {
+      this.movePlayer();
+      console.log(data);
+    } else if (data["status"] == "invalid") {
+      console.error(data["message"]);
+    }
   }
 
-  onClose(event) {}
+  onClose(event) {
+    this.unsetKeyListeners();
+    this.gameRunning = false;
+  }
 
-  movePlayer(direction) {
-    this.socket.send(JSON.stringify({ move: direction }));
+  movePlayer() {
+    if (!this.p1Direction && !this.p2Direction) {
+      return;
+    }
+    let message = JSON.stringify({
+      l: this.p1Direction,
+      r: this.p2Direction,
+    });
+    this.socket.send(message);
+  }
+
+  gameAction(action) {
+    let message;
+    if (action == "start") {
+      message = JSON.stringify({
+        start: true,
+      });
+      this.setKeyListeners();
+      this.gameRunning = true;
+    } else {
+      message = JSON.stringify({
+        stop: true,
+      });
+      this.unsetKeyListeners();
+      this.gameRunning = false;
+    }
+    this.socket.send(message);
+  }
+
+  handleKeyDown(event) {
+    let keyPressed = event.key;
+
+    if (!this.p1Direction) {
+      if (keyPressed === this.P1_UP) {
+        this.p1Direction = "u";
+        return;
+      } else if (keyPressed === this.P1_DOWN) {
+        this.p1Direction = "d";
+        return;
+      }
+    }
+    if (!this.p2Direction) {
+      if (keyPressed === this.P2_UP) {
+        this.p2Direction = "u";
+        return;
+      } else if (keyPressed === this.P2_DOWN) {
+        this.p2Direction = "d";
+        return;
+      }
+    }
+  }
+
+  handleKeyUp(event) {
+    let keyPressed = event.key;
+
+    if (keyPressed === this.P1_DOWN || keyPressed === this.P1_UP) {
+      this.p1Direction = null;
+    } else if (keyPressed === this.P2_DOWN || keyPressed === this.P2_UP) {
+      this.p2Direction = null;
+    }
+  }
+
+  setKeyListeners() {
+    document.addEventListener("keydown", this.handleKeyDown);
+    document.addEventListener("keyup", this.handleKeyUp);
+  }
+
+  unsetKeyListeners() {
+    document.removeEventListener("keydown", this.handleKeyDown);
+    document.removeEventListener("keyup", this.handleKeyUp);
   }
 }
