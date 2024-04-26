faf72d2786d67f7da46da72ec8408cd77da7f70b
gsistelos <gabrielsistelos@gmail.com>
2024-04-26 13:35:54 -0300

    fix errors when initializing container

--- django/ft_transcendence/settings.py
+++ django/ft_transcendence/settings.py
@@ -47,7 +47,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework',
 ]
 
 AUTH_USER_MODEL = "user.User"
@@ -66,11 +65,6 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-
-REST_FRAMEWORK = {
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10
-}
 
 ROOT_URLCONF = 'ft_transcendence.urls'
 
