5ddd88359bd62af076b01224df89f0020697df99
gsistelos <gabrielsistelos@gmail.com>
2024-05-08 14:23:01 -0300

    removed routes

--- django/relations/urls.py
+++ django/relations/urls.py
@@ -6,9 +6,4 @@ urlpatterns = [
     path('friends/invites-sent/', views.friend_invites_sent),
     path('friends/invites-received/', views.friend_invites_received),
     path('friends/block-list/', views.block_list),
-    path('search-user/', views.search_user),
-
-    # Temporary routes for adding/blocking users
-    path('friends/send-invite/', views.send_friend_invites),
-    path('friends/block/', views.block_user),
 ]
