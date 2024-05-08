ca12fe116806007bfc11fbbdc2d172bec401f74e
gsistelos <gabrielsistelos@gmail.com>
2024-05-08 14:20:15 -0300

    search in home, added block list to friends page

--- django/pong/views.py
+++ django/pong/views.py
@@ -1,8 +1,21 @@
+from user.models import User
+from django.shortcuts import get_object_or_404, redirect
 from .utils import render_component
 
 # Create your views here.
 
 
 def home(request):
-    if request.method == "GET":
-        return render_component(request, "home.html", "content")
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        try:
+            user = get_object_or_404(User, username=username)
+        except:
+            return render_component(request, 'search_user_form.html', 'form', {
+                'error': 'User not found',
+                'username': username # So user doesn't have to re-type
+            }, status=404)
+
+        return redirect('/profile/' + user.username)
+
+    return render_component(request, 'home.html', 'content')
--- django/relations/views.py
+++ django/relations/views.py
@@ -43,22 +43,6 @@ def friend_invites_received(request):
     return render_component(request, 'friend_invites_received.html', 'content')
 
 
-def search_user(request):
-    if request.method == 'POST':
-        username = request.POST.get('username')
-        try:
-            user = get_object_or_404(User, username=username)
-        except:
-            return render_component(request, 'search_user_form.html', 'form', {
-                "error": "User not found",
-                "username": username # So user doesn't have to re-type
-            }, status=404)
-
-        return redirect("/profile/" + user.username)
-
-    return render_component(request, 'search_user.html', 'content')
-
-
 def block_list(request):
     if request.method == 'POST':
         # Unblock user
@@ -67,48 +51,3 @@ def block_list(request):
         request.user.unblock_user(user)
 
     return render_component(request, 'block_list.html', 'content')
-
-
-def send_friend_invites(request):
-    if request.method == 'POST':
-        try:
-            
-
-            return render_component(request, 'send_friend_invites.html', 'content', {
-                'success': 'Friend invite sent!'
-            })
-        except Exception as e:
-            if type(e) == Http404:
-                error = 'User not found'
-            else:
-                error = e.message
-
-            return render_component(request, 'send_friend_invites.html', 'content', {
-                'username': name, # So user doesn't have to re-type the username
-                'error': error
-            })
-
-    return render_component(request, 'send_friend_invites.html', 'content')
-
-
-def block_user(request):
-    if request.method == 'POST':
-        try:
-            name = request.POST.get('username')
-            user = get_object_or_404(User, username=name)
-            request.user.block_user(user)
-            return render_component(request, 'block_user.html', 'content', {
-                'success': 'User blocked!'
-            })
-        except Exception as e:
-            if type(e) == Http404:
-                error = 'User not found'
-            else:
-                error = e.message
-
-            return render_component(request, 'block_user.html', 'content', {
-                'username': name, # So user doesn't have to re-type the username
-                'error': error
-            })
-
-    return render_component(request, 'block_user.html', 'content')
