9f487b10a869d1eb77d13d485d6d943ac6b96c36
gabrielclcardoso <gabrielclcardoso@gmail.com>
2024-05-31 21:17:38 -0300

    feat: add link to profile on chat

--- django/user/urls.py
+++ django/user/urls.py
@@ -12,31 +12,30 @@ urlpatterns = [
     path('match_history/', views.match_history),
 
     path('profile/', views.my_profile),
-    path('profile/<str:username>/', views.user_profile),
+    path('profile/<str:username>/', views.user_profile, name="userProfile"),
     path('edit_profile/', views.edit_profile),
 
     path('change_password/', views.change_password),
     path('verify_email/', views.verify_email),
 
-	path(
-    'generate/totp/factor/',
-    views.generate_totp_factor,
-    name='generate_totp_factor'
+    path(
+        'generate/totp/factor/',
+        views.generate_totp_factor,
+        name='generate_totp_factor'
     ),
-	path(
-    'list/totp/factors/',
-    views.list_totp_factors,
-    name='list_totp_factors'
+    path(
+        'list/totp/factors/',
+        views.list_totp_factors,
+        name='list_totp_factors'
     ),
-	path(
-    'verify/totp/factor/',
-    views.verify_totp_factor,
-    name='verify_totp_factor'
+    path(
+        'verify/totp/factor/',
+        views.verify_totp_factor,
+        name='verify_totp_factor'
     ),
-	path(
-    'validate/totp/token/',
-    views.validate_totp_token,
-    name='validate_totp_token'
+    path(
+        'validate/totp/token/',
+        views.validate_totp_token,
+        name='validate_totp_token'
     ),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) # For Hentai images
-
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
