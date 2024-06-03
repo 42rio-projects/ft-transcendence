619ce9919a8e96ad50e303d413c071fedec3a345
Daniel Vargas <daniel_v93@hotmail.com>
2024-06-02 21:29:09 -0300

    update

--- django/app/static/js/script.js
+++ django/app/static/js/script.js
@@ -4,121 +4,74 @@ let messageSocket;
 let localTournamentSocket;
 let onlineTournamentSocket;
 
-// Fetches with X-Transcendence header
-// inserts response HTML into target element
-// returns response URL for history navigation
-async function fetchData(
-  url,
-  { method, body } = { method: "GET", body: null },
-) {
-  const response = await fetch(url, {
-    headers: {
-      "X-Transcendence": true,
-    },
-    method,
-    body,
-  });
+async function fetchData(url, options = {}) {
+  options.headers = options.headers || {};
+  options.headers["X-Transcendence"] = true;
+
+  const response = await fetch(url, options);
 
   const target_id = response.headers.get("X-Target-Id");
-  const html = await response.text();
-
-  document.getElementById(target_id).innerHTML = html;
+  const target = document.getElementById(target_id);
+  if (target) {
+    target.innerHTML = await response.text();
+  } else {
+    console.error("Could not find target element with id: " + target_id);
+  }
 
   handleSockets(response.url);
 
   return response;
 }
 
-function handleFormSubmit(event) {
-  event.preventDefault();
-
-  const form = event.target;
-
-  fetchData(form.action, {
-    method: form.method,
-    body: new FormData(form),
-  })
-    .then((response) => {
-      // If the form returns a different URL, update the history
-      if (response.url !== form.action) {
-        history.pushState({ url: response.url }, null, response.url);
-      }
-    })
-    .catch((error) => console.error(error));
+async function navigate(url) {
+  const response = await fetchData(url);
+  if (response.url != window.location.href) {
+    history.pushState({ url: response.url }, null, response.url);
+  }
 }
 
 // Navigation with history (forward and back buttons)
-function handleNavigation(event) {
-  const url = event.state ? event.state.url : window.location.href;
-
-  fetchData(url).catch((error) => console.error(error));
-}
-
-window.addEventListener("popstate", handleNavigation);
+window.addEventListener("popstate", async (event) => {
+  event.preventDefault();
+  await fetchData(event.state ? event.state.url : window.location.href);
+});
 
 // Navigation with links (anchor tags)
-function handleLinkClick(event) {
-  event.preventDefault();
-
-  const url = event.target.href;
-
-  fetchData(url)
-    .then((response) => {
-      history.pushState({ url: response.url }, null, response.url);
-    })
-    .catch((error) => console.error(error));
-}
-
-document.addEventListener("click", (event) => {
+document.addEventListener("click", async (event) => {
   if (event.target.tagName === "A") {
-    handleLinkClick(event);
+    event.preventDefault();
+    await navigate(event.target.href);
   }
 });
 
-async function handleLogin(event) {
+async function handleFormSubmit(event) {
   event.preventDefault();
 
   const form = event.target;
-
-<<<<<<< HEAD:django/app/static/js/script.js
-  constructor() {
-    this.socket = new WebSocket(
-      "wss://" + window.location.host + "/ws/local-game/",
-    );
-    this.socket.onmessage = this.onMessage.bind(this);
-    this.socket.onclose = this.onClose.bind(this);
-    this.handleKeyUp = this.handleKeyUp.bind(this);
-    this.handleKeyDown = this.handleKeyDown.bind(this);
-    this.pressedKeys[this.P1_UP] = false;
-    this.pressedKeys[this.P2_UP] = false;
-    this.pressedKeys[this.P1_DOWN] = false;
-    this.pressedKeys[this.P2_DOWN] = false;
-=======
   const response = await fetchData(form.action, {
     method: form.method,
     body: new FormData(form),
   });
 
+  if (response.url != form.action) {
+    history.pushState({ url: response.url }, null, response.url);
+  }
+
+  return response;
+}
+
+async function handleLogin(event) {
+  const response = await handleFormSubmit(event);
   if (response.status === 200) {
-    const params = new URLSearchParams(window.location.search);
-    const next = params.get("next") || "/";
-
-    history.pushState({ url: next }, null, next);
-
     statusSocket.connect();
->>>>>>> develop:django/static/js/script.js
   }
 }
 
-async function handleLogout(event) {
-  event.preventDefault();
-
-  const response = await fetch("/logout/")
-
+async function handleLogout(url) {
+  const response = await fetchData(url);
   if (response.status === 200) {
+    history.pushState({ url: response.url }, null, response.url);
     statusSocket.close();
-    await fetchData("/");
-    history.pushState({ url: "/" }, null, "/");
   }
 }
 
